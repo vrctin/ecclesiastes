04bfc67aee8bc382b028c04292cb3b18bcbc20e2
Victor Razvan <vr.ctin@gmail.com>
2019-08-13 18:39:03 +0300

    cum sa NU rezolvi harta

--- src/app/game/match-view/match-view.component.ts
+++ src/app/game/match-view/match-view.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { PanZoomConfig, PanZoomAPI, PanZoomModel } from 'ng2-panzoom';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+
+import panzoom from "panzoom";
 
 @Component({
   selector: 'app-match-view',
@@ -7,20 +8,20 @@ import { PanZoomConfig, PanZoomAPI, PanZoomModel } from 'ng2-panzoom';
   styleUrls: ['./match-view.component.css']
 })
 export class MatchViewComponent implements OnInit {
+  @ViewChild('gameMap', { static: false }) scene: ElementRef;
+  panZoomController;
+  zoomLevels: number[];
 
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit() {
   }
 
-  public panzoomConfig: PanZoomConfig = new PanZoomConfig({
-    zoomLevels: 10,
-    scalePerZoomLevel: 2.0,
-    zoomStepDuration: 0.2,
-    freeMouseWheelFactor: 0.01,
-    zoomToFitZoomLevelFactor: 0.9,
-    dragMouseButton: 'left',
-    keepInBounds: true
-  });
+  ngAfterViewInit(){
+    this.zoomLevels = [0.1, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.5, 3];
+    this.currentZoomLevel = this.zoomLevels[4];
+    this.panZoomController = panzoom(this.scene.nativeElement);
+  }
 
 }
