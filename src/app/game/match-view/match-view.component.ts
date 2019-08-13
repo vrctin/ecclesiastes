e4b9acb7760324ef5665bfce765cbd94b079edc8
Victor Razvan <vr.ctin@gmail.com>
2019-08-13 19:27:02 +0300

    Panzoom la svg

--- src/app/game/match-view/match-view.component.ts
+++ src/app/game/match-view/match-view.component.ts
@@ -1,27 +1,21 @@
 import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
-
-import panzoom from "panzoom";
+import * as svgPanZoom from "svg-pan-zoom";
 
 @Component({
   selector: 'app-match-view',
   templateUrl: './match-view.component.html',
   styleUrls: ['./match-view.component.css']
 })
+
 export class MatchViewComponent implements OnInit {
   @ViewChild('gameMap', { static: false }) scene: ElementRef;
-  panZoomController;
-  zoomLevels: number[];
-
 
   constructor() {}
-
   ngOnInit() {
   }
 
   ngAfterViewInit(){
-    this.zoomLevels = [0.1, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.5, 3];
-    this.currentZoomLevel = this.zoomLevels[4];
-    this.panZoomController = panzoom(this.scene.nativeElement);
+    var panZoomMap = svgPanZoom('#gameMap');
   }
 
 }
