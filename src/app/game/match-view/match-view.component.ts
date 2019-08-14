9a4d986b274f228bbe2a4a3d76649892b30f38a9
Victor Razvan <vr.ctin@gmail.com>
2019-08-14 14:12:47 +0300

    Implementat limita la panning (basic)

--- src/app/game/match-view/match-view.component.ts
+++ src/app/game/match-view/match-view.component.ts
@@ -18,6 +18,25 @@ export class MatchViewComponent implements OnInit {
     console.log("Mouse down on element with id " + event.toElement.id)
   }
 
+  public beforePan = function(oldPan, newPan){
+    var stopHorizontal = false;
+    var stopVertical = false;
+    var gutterWidth = 400;
+    var gutterHeight = 400;
+    // Computed variables
+    var sizes = this.getSizes();
+    var leftLimit = -((sizes.viewBox.x + sizes.viewBox.width) * sizes.realZoom) + gutterWidth;
+    var rightLimit = sizes.width - gutterWidth - (sizes.viewBox.x * sizes.realZoom);
+    var topLimit = -((sizes.viewBox.y + sizes.viewBox.height) * sizes.realZoom) + gutterHeight;
+    var bottomLimit = sizes.height - gutterHeight - (sizes.viewBox.y * sizes.realZoom);
+
+    var customPan = {};
+    customPan.x = Math.max(leftLimit, Math.min(rightLimit, newPan.x));
+    customPan.y = Math.max(topLimit, Math.min(bottomLimit, newPan.y));
+
+    return customPan;
+  };
+
   ngAfterViewInit(){
     var panZoomMap = svgPanZoom('#gameMap', {
       panEnabled: true,
@@ -27,7 +46,8 @@ export class MatchViewComponent implements OnInit {
       center: true,
       fit: true,
       refreshRate: 'auto',
-      zoomScaleSensitivity: 0.6
+      zoomScaleSensitivity: 0.6,
+      beforePan: this.beforePan
     });
   }
 
